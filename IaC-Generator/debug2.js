c0dba3870c49b9e979bf4b6f4bbec9fbf8464aee
doncalli <jingleole@gmail.com>
2026-07-22 22:39:13 -0500

    Update debug2.js

--- IaC-Generator/debug2.js
+++ IaC-Generator/debug2.js
@@ -3,5 +3,13 @@ const path = require('path');
 const filePath = path.join(__dirname, 'db/azure-repo/generated', 'addons/microsoft.addons/2017-05-15/types.json');
 const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
-// Print the 18th item (index 17) to see what a resource looks like
-console.log("Item 17:", JSON.stringify(data[17], null, 2));
+// Look at item 7 (the actual body)
+console.log("Item 7 (The actual body):", JSON.stringify(data[7], null, 2).substring(0, 1000) + "\n\n... (truncated)");
+
+// Check if it has properties
+if (data[7].properties) {
+    const keys = Object.keys(data[7].properties);
+    console.log("PROPERTIES FOUND! Keys:", keys);
+} else {
+    console.log("No direct properties. It might be another $ref. Full item 7:", JSON.stringify(data[7]));
+}
