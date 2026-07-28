0a742a39f946f08a903d5817005d08fa98412502
doncalli <jingleole@gmail.com>
2026-07-28 12:57:06 -0500

    updates

--- Iac-Generator-v2/debug.js
+++
@@ -1,4 +0,0 @@
-const j = require('./db/azure-repo/generated/index.json');
-const firstKey = Object.keys(j.resources)[0];
-console.log('Key:', firstKey);
-console.log('Ref:', j.resources[firstKey]['$ref']);
